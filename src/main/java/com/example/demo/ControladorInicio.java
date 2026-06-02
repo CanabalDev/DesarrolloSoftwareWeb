b7add2577e79c96e29ef9fffd29dffe81b349254
canabaldev1 <canabal.dev1@gmail.com>
2026-06-01 23:25:10 -0500

    Intercambio de datos

--- src/main/java/com/example/demo/ControladorInicio.java
+++ src/main/java/com/example/demo/ControladorInicio.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -11,9 +13,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 public class ControladorInicio {
-
+    
+    @Value("${index.mensaje}")
+    String dato;
+    
     @GetMapping("/")
-    public String inicio(){
+    public String inicio(Model modelo){
+        
+        String mensaje = "Saludos MVC";
+        modelo.addAttribute("mensaje",mensaje);
+        modelo.addAttribute("dato", dato);
+        
         log.info("Ejecutando el controlador de inicio");
         return "index";
     }
