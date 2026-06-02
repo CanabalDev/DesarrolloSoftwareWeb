d3d324e6f5bbfad71aa29b3bfebe7fd1bc801c6d
canabaldev1 <canabal.dev1@gmail.com>
2026-06-01 23:42:58 -0500

    Agregado el intercambio de objetos

--- src/main/java/com/example/demo/ControladorInicio.java
+++ src/main/java/com/example/demo/ControladorInicio.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.modelo.Equipo;
+
 /**
  *
  * @author lm-carlos
@@ -13,16 +15,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 public class ControladorInicio {
-    
+
     @Value("${index.mensaje}")
     String dato;
-    
+
     @GetMapping("/")
-    public String inicio(Model modelo){
-        
+    public String inicio(Model modelo) {
+
         String mensaje = "Saludos MVC";
-        modelo.addAttribute("mensaje",mensaje);
+        modelo.addAttribute("mensaje", mensaje);
         modelo.addAttribute("dato", dato);
+
+        Equipo inter = new Equipo();
+
+        inter.setNombre("Inter de Milan");
+        inter.setEslogan("Forza Inter");
+        inter.setTecnico("Cristian Chivu");
+        inter.setPais("Italia");
+        inter.setCiudad("Milan");
+        inter.setCategoria("Serie A");
+
+        inter.setNumGoles(89);
+        inter.setNumPartidosJugados(38);
+        inter.setNumPartidosGanados(27);
+        inter.setNumCampeonatos(21);
+        inter.setNumExpulsiones(3);
+        inter.setNumEmpates(6);
+
+        modelo.addAttribute("equipo",inter);
         
         log.info("Ejecutando el controlador de inicio");
         return "index";
---
+++ src/main/java/com/example/demo/modelo/Equipo.java
@@ -0,0 +1,31 @@
+package com.example.demo.modelo;
+
+import lombok.Data;
+
+/**
+ *
+ * @author lm-carlos
+ */
+
+@Data
+public class Equipo {
+
+    private String nombre;
+    private String eslogan;
+    private String tecnico;
+    private String pais;
+    private String ciudad;
+    private String categoria;
+
+    private int numGoles;
+    private int numPartidosJugados;
+    private int numPartidosGanados;
+    private int numCampeonatos;
+    private int numExpulsiones;
+    private int numEmpates;
+
+ 
+    
+    
+    
+}
