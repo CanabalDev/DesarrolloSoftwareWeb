667795e861da7ecf8f0670513ffeec7b9784592b
canabaldev1 <canabal.dev1@gmail.com>
2026-06-01 23:11:51 -0500

    Creada vista

--- src/main/java/com/example/demo/ControladorInicio.java
+++ src/main/java/com/example/demo/ControladorInicio.java
@@ -1,21 +1,20 @@
 package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author lm-carlos
  */
-@RestController
+@Controller
 @Slf4j
 public class ControladorInicio {
 
     @GetMapping("/")
     public String inicio(){
-        System.out.println("ENTRO AL CONTROLADOR");
         log.info("Ejecutando el controlador de inicio");
-        return "Este es el inicio! v2.0";
+        return "index";
     }
 }
